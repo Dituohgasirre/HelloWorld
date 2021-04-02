22ec3390df8a1b6085822c3d374fb75b50d53ed9
Dituohgasirre <linfeiji4729289@126.com>
2021-04-02 17:58:23 +0800

    2016/07/20 周三17:58:23.57

--- src/qingke/Recursion1.java
+++ src/qingke/Recursion1.java
@@ -86,6 +86,18 @@ public class Recursion1 {
 		}
 		  
 	}
+	public int countX(String str) {
+		if(str.length()<1){
+			return 0;
+		}
+		if(str.charAt(0)=='x'){
+			return 1+countX(str.substring(1));
+		}else{
+			return countX(str.substring(1));
+		}
+		
+		 
+	}
 
 
 
