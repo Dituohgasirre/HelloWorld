d9c1bda1481e0357415036912e8753515b800884
Dituohgasirre <linfeiji4729289@126.com>
2021-04-12 21:02:36 +0800

    2016/07/30 周六21:02:36.47

---
+++ src/qingke1/Lamp.java
@@ -0,0 +1,21 @@
+package qingke1;
+
+public enum Lamp {
+	RED("ºìµÆ","Í£"),
+	GREEN("ÂÌµÆ","ÐÐ"),
+	YELLOW("»ÆµÆ","Âý");
+	private String color;
+	private String destription;
+	Lamp(String color, String description){
+		this.color=color;
+		this.destription=description;
+	}
+	public String getColor(){
+		return color;
+	}
+	public String getDescription(){
+		return destription;
+		
+	}
+	
+}
