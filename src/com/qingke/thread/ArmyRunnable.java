babc3723cc9f44fdd97ed01ef62acf6c96bf83df
Dituohgasirre <linfeiji4729289@126.com>
2021-04-16 20:52:16 +0800

    2016/08/03 周三20:52:16.34

---
+++ src/com/qingke/thread/ArmyRunnable.java
@@ -0,0 +1,20 @@
+package com.qingke.thread;
+//军队线程
+//模拟作战双方的行为
+public class ArmyRunnable implements Runnable {
+	//volatile保证了线程可以正确地读取其他线程写入的值
+	volatile boolean keepRunning=true;
+	@Override
+	public void run() {
+		while(keepRunning){
+			for(int i=0;i<5;i++){
+				System.out.println(Thread.currentThread().getName()+"进攻对方["+i+"]");
+				//让出来处理器时间，下次谁进攻还不一定呢
+				Thread.yield();
+			}
+		}
+		System.out.println(Thread.currentThread().getName()+"结束了战斗！");
+
+	}
+
+}
---
+++ src/com/qingke/thread/Stage.java
@@ -0,0 +1,40 @@
+package com.qingke.thread;
+/**
+ * 隋唐演义大舞台
+ * @author wcd
+ *
+ */
+public class Stage extends Thread {
+	public void run(){
+		ArmyRunnable armyTaskOfSuiDynasty =new ArmyRunnable();
+		ArmyRunnable armyTaskOfRevolt =new ArmyRunnable();
+		
+		Thread armyOfSuiDynasty = new Thread(armyTaskOfSuiDynasty,"随军");
+		Thread armyOfRevolt = new Thread(armyTaskOfRevolt,"农民起义军");
+		
+		armyOfSuiDynasty.start();
+		armyOfRevolt.start();
+		
+		try {
+			Thread.sleep(50);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		armyTaskOfSuiDynasty.keepRunning=false;
+		armyTaskOfRevolt.keepRunning=false;
+		
+		try {
+			armyOfRevolt.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Stage().start();
+	}
+
+}
